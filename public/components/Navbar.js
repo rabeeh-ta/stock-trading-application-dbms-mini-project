5b9d33b89a6e0fe01c9e11e50a276245f3559ed2
Rabeeh T A <rabeehchemnad@gmail.com>
2023-01-03 23:29:22 +0530

    created all pages and connected to nav bar

--- public/components/Navbar.js
+++ public/components/Navbar.js
@@ -1,20 +1,51 @@
 function Navbar(props) {
+  function routingFunction(page) {
+    window.location.replace(`./${page}.html`);
+  }
   return (
-    <nav>
-      {props.page ? (
-        <ul>
-          <li className={props.page == 'Home' ? 'activeTab' : ''}>Home</li>
-          <li className={props.page == 'Trade' ? 'activeTab' : ''}>Trade</li>
-          <li className={props.page == 'Portfolio' ? 'activeTab' : ''}>
-            Portfolio
-          </li>
-          <li className={props.page == 'Funds' ? 'activeTab' : ''}>Funds</li>
-        </ul>
-      ) : (
-        <ul></ul>
-      )}
+    <div className="navbar">
+      <nav>
+        {props.page ? (
+          <ul>
+            <li
+              className={props.page == 'Home' ? 'activeTab' : ''}
+              onClick={() => {
+                routingFunction('home');
+              }}
+            >
+              Home
+            </li>
+            <li
+              className={props.page == 'Trade' ? 'activeTab' : ''}
+              onClick={() => {
+                routingFunction('trade');
+              }}
+            >
+              Trade
+            </li>
+            <li
+              className={props.page == 'Portfolio' ? 'activeTab' : ''}
+              onClick={() => {
+                routingFunction('portfolio');
+              }}
+            >
+              Portfolio
+            </li>
+            <li
+              className={props.page == 'Funds' ? 'activeTab' : ''}
+              onClick={() => {
+                routingFunction('funds');
+              }}
+            >
+              Funds
+            </li>
+          </ul>
+        ) : (
+          <ul></ul>
+        )}
 
-      <img src="./src/logo.png" alt="" />
-    </nav>
+        <img src="./src/logo.png" alt="" />
+      </nav>
+    </div>
   );
 }
